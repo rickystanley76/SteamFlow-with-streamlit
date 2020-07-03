fd5384996ab211cfabae8841a02f622ba1108863
Ricky D'Cruze <rickystanley76@gmail.com>
2020-07-03 14:01:48 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
 import pickle
+import os
+
+
+
 
 
 st.title('SteamFlow Prediction APP for a Papermil ')
@@ -8,28 +12,70 @@ from PIL import Image
 image = Image.open('papermil1000.jpg')
 st.image(image, caption='Paper mill- Paper production (Own image)', width=400)
 
-st.write("""
+# EDA
+my_dataset = "papermil_water_flow.csv"
+
+# To Improve speed and cache data
+@st.cache(persist=True)
+def explore_data(dataset):
+	df = pd.read_csv(os.path.join(dataset))
+	return df 
+
+
+# Show Entire Dataframe
+if st.checkbox("Show DataFrame used in the Model Building"):
+	data = explore_data(my_dataset)
+	st.dataframe(data)
+
+# Show Description
+if st.checkbox("Show All Column Names"):
+	data = explore_data(my_dataset)
+	st.text("Columns:")
+	st.write(data.columns)
+    
+# Dimensions
+data_dim = st.radio('What Dimension Do You Want to Show',('Rows','Columns'))
+if data_dim == 'Rows':
+	data = explore_data(my_dataset)
+	st.text("Showing Length of Rows")
+	st.write(len(data))
+if data_dim == 'Columns':
+	data = explore_data(my_dataset)
+	st.text("Showing Length of Columns")
+	st.write(data.shape[1])
+
+
+if st.checkbox("Show Summary of Dataset"):
+	data = explore_data(my_dataset)
+	st.write(data.describe())
+
+
+
+showdescription = st.checkbox('Show Project Description')
+
+if showdescription:
+    st.write("""
 # Description
 
 This app predicts the **SteamFlow** of a PaperRoll in PaperMill!
 
 During the production of a paper roll in the paper mill, it takes around **17-20** tons of water/hour(Depends on the size of the roll).
-There are 403 parameters nobs are all around the paper machine, which measures different parameters during the production. Among them 23 of them are
+There are 403 parameters nobs are all around the paper machine, which measures different parameters during the production. Among them 11 of them are
 more important that has good effect on the water uses(SteamFlow) parapeter.
 
-With this app production manager can check how much water will be used to produce a paper roll and set the parameters to those 23 nobs.
+With this app production manager can check how much water will be used to produce a paper roll and set the parameters to those 12 nobs.
 
 #  Tools used:
 
 We have used Random Forest regressor to make the model with hyperparameter tuning. 
 
-Data we had collected from a paper mill open data. The data consists of 3941 records with 24 features. 
+Data we had collected from a paper mill open data. The data consists of 3941 records with 11(independent) 1(dependent) features. 
 
 Used scikit learn.
 
 """)
 
-st.sidebar.header('User Input Features')
+st.sidebar.header('User Input Features- 11 parameters ')
 
 # Collects user input features into dataframe
 uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
@@ -37,56 +83,44 @@ if uploaded_file is not None:
     input_df = pd.read_csv(uploaded_file)
 else:
     def user_input_features():
-        starch_mixing_tank23top_ply = st.sidebar.slider('Starch mixing in TANK23 top Ply', 21.1,61.9,34.2,0.2)
+   
         pulp_to_mixing_tank23 = st.sidebar.slider('Pulp to mixing TANK23', 131.0,223.0,175.0,0.2)
-        power_power_station24 = st.sidebar.slider('Power of POWERSTATION24', 13.0,342.0,250.0,0.2)
-                   
-              
-        recycled_liquid_top_ply = st.sidebar.slider('Recycled liquid of Top_ply', 0.0,2.0,1.0,0.1)
-        sum_bleached_pulp = st.sidebar.slider('Sum of Bleached Pulp', 6.0,10.0,7.0,0.2)
+
         flow_after_machine_chest21 = st.sidebar.slider('Flow after Machine_Chest21', 2148.0,3700.0,2888.0,0.5)
-        steam_pressure_group1 = st.sidebar.slider('Steam pressure of group1', 6.0,104.0,80.0,0.2)
-        steam_pressure_group2 = st.sidebar.slider('Steam pressure of group2', 39.0,176.0,156.0,0.2)
-        level_condensate_bucket1 = st.sidebar.slider('level Condensate Bucket1', 26.0,531.0,330.0,0.2)
-        level_condensate_bucket1_valve_position = st.sidebar.slider('level Condensate_bucket1_valve_position', 26.0,531.0,330.0,0.2)  
-        level_condensate_bucket2_valve_position = st.sidebar.slider('level condensate_bucket2_valve_position', 26.0,531.0,330.0,0.2) 
+
+        level_condensate_bucket1_valve_position = st.sidebar.slider('level condensate bucket1 valve position', 26.0,531.0,330.0,0.2)
+        
         steam_group3_pressure = st.sidebar.slider('Steam group3 pressure', 26.0,531.0,330.0,0.2)                
         steam_group4under_pressure = st.sidebar.slider('Steam group4 under_pressure', 26.0,531.0,330.0,0.2)                
-        steam_pressure4_over = st.sidebar.slider('Steam pressure4 over', 142.0,294.0,238.0,0.2)
+       
         pressure_yankee_cylinder = st.sidebar.slider('Pressure of yankee cylinder', 143.0,295.0,237.0,0.2)
         steam_group5under_pressure = st.sidebar.slider('Steam Group5under Pressure', 143.0,287.0,237.0,0.2)
+        
         steam_pressure5_over = st.sidebar.slider('Steam pressure5 over', 143.0,290.0,237.0,0.2)
-        pressure_condensate_bucket2 = st.sidebar.slider('Pressure_condensate_bucket2', 39.0,188.0,158.0,0.2)
+
         pressure_condensate_bucket5 = st.sidebar.slider('Pressure_condensate_bucket5', 2.0,100.0,75.0,0.2)
         production_paper_machine2 = st.sidebar.slider('Production PAPERMACHINE2', 0.0,14.0,10.0,0.2)
         dry_production_vira = st.sidebar.slider('Dry production Vira', 115.0,205.0,160.0,0.2)
-        total_starch_paper_machine2 = st.sidebar.slider('Total starch of PAPERMACHINE2', 37.0,95.0,57.0,0.2)
+   
        
        
         
-        data = {'starch_mixing_tank23top_ply': starch_mixing_tank23top_ply,
-                'pulp_to_mixing_tank23': pulp_to_mixing_tank23,
-                'power_power_station24': power_power_station24,
+        data = {'pulp_to_mixing_tank23': pulp_to_mixing_tank23,
                 
-                'recycled_liquid_top_ply': recycled_liquid_top_ply,
-                'sum_bleached_pulp': sum_bleached_pulp,
                 'flow_after_machine_chest21': flow_after_machine_chest21,
-                'steam_pressure_group1': steam_pressure_group1,
-                'steam_pressure_group2': steam_pressure_group2,
-                'level_condensate_bucket1': level_condensate_bucket1,
-                'steam_pressure4_over': steam_pressure4_over,
-                'pressure_yankee_cylinder': pressure_yankee_cylinder,
+                
+                'level_condensate_bucket1_valve_position': level_condensate_bucket1_valve_position,
+                'steam_group3_pressure': steam_group3_pressure,
                 'steam_group5under_pressure': steam_group5under_pressure,
+                'pressure_yankee_cylinder': pressure_yankee_cylinder,
+                'steam_group4under_pressure': steam_group4under_pressure,
+                
                 'steam_pressure5_over': steam_pressure5_over,
-                'pressure_condensate_bucket2': pressure_condensate_bucket2,
+              
                 'pressure_condensate_bucket5': pressure_condensate_bucket5,
                 'production_paper_machine2': production_paper_machine2,
-                'dry_production_vira': dry_production_vira,
-                'total_starch_paper_machine2': total_starch_paper_machine2,
-                'level_condensate_bucket1_valve_position': level_condensate_bucket1_valve_position,
-                'level_condensate_bucket2_valve_position': level_condensate_bucket2_valve_position,
-                'steam_group3_pressure': steam_group3_pressure,
-                'steam_group4under_pressure': steam_group4under_pressure
+                'dry_production_vira': dry_production_vira
+          
                 
                 
                 
@@ -124,13 +158,16 @@ prediction = load_rf_model.predict(input_df)
 
 
 st.write("""
-# Prediction of SteamFlow(tons/hour)
+# Result-----Prediction of SteamFlow(tons/hour)
 """)
 st.write(prediction)
 
 #st.subheader('Prediction Probability')
 #st.write(prediction_proba)
 
+if st.button("About"):
+        st.text("Model-Built with Random forest regressor")
+        st.text("by: Ricky D'Cruze, 0762336239, Västerås, Sweden")
+        
 
 
-st.subheader('Model and UI developed by : Ricky D Cruze')
--- papermil-model-building.py
+++ papermil-model-building.py
@@ -1,38 +1,25 @@
 import pandas as pd
+import numpy as np
+
 papermil = pd.read_csv('papermil_water_flow.csv')
 
 # Ordinal feature encoding
 # https://www.kaggle.com/pratik1120/penguin-dataset-eda-classification-and-clustering
 df = papermil.copy()
-#target = 'steamflow'
-#encode = ['sex','island']
 
-#for col in encode:
- #   dummy = pd.get_dummies(df[col], prefix=col)
-  #  df = pd.concat([df,dummy], axis=1)
-  #  del df[col]
+final_df = df[['pulp_to_mixing_tank23', 'flow_after_machine_chest21', 'level_condensate_bucket1_valve_position', 'steam_group3_pressure',
+              'steam_group4under_pressure', 'pressure_yankee_cylinder', 'steam_group5under_pressure',
+              'steam_pressure5_over', 'pressure_condensate_bucket5', 'production_paper_machine2', 'dry_production_vira',
+              'steamflow']]
 
-#target_mapper = {'Adelie':0, 'Chinstrap':1, 'Gentoo':2}
-#def target_encode(val):
- #   return target_mapper[val]
-
-#df['species'] = df['species'].apply(target_encode)
-
-# Separating X and y
-X = df.drop('steamflow', axis=1)
-y = df['steamflow']
+X = final_df.drop('steamflow', axis=1)
+y = final_df['steamflow']
 
 # Build random forest model
-#from sklearn.ensemble import RandomForestClassifier
-#clf = RandomForestClassifier()
-#clf.fit(X, Y)
+# With Random Forest Regressor
 
 from sklearn.model_selection import train_test_split
-#from sklearn import metrics 
-#training set 80%
-#testing set 20%
-
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size= 0.20)
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size= 0.30)
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import RandomizedSearchCV # Create the parameter grid based on the results of random search 
@@ -46,13 +33,56 @@ param_grid = {
 }
 # Create a based model
 rf = RandomForestRegressor() # Instantiate the grid search model
-random_search = RandomizedSearchCV(estimator = rf, param_distributions = param_grid, cv = 5, n_jobs = -1, verbose = 2)
-model_fit_RF= random_search.fit(X_train, y_train)
+random_search_rf = RandomizedSearchCV(estimator = rf, param_distributions = param_grid, cv = 10, n_jobs = -1, verbose = 2)
+model_fit_RF= random_search_rf.fit(X_train, y_train)
 print(model_fit_RF.best_params_)
 ##Testing the model
 test_predict_RF = model_fit_RF.predict(X_test)
 
 
+from sklearn import metrics
+
+print('RF Mean Absolute Error:', metrics.mean_absolute_error(y_test, test_predict_RF))
+print('RF Mean Squared Error:', metrics.mean_squared_error(y_test, test_predict_RF))
+print('RF R2:', metrics.r2_score(y_test, test_predict_RF))
+print('RF Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, test_predict_RF)))
 # Saving the model
 import pickle
 pickle.dump(model_fit_RF, open('papermil_rf.pkl', 'wb'))
+
+
+#DT with hypeparmeter tuning
+from sklearn.tree import DecisionTreeRegressor
+## Hyper Parameter Optimization
+
+params={
+ "splitter"    : ["best","random"] ,
+ "max_depth"        : [ 3, 4, 5, 6, 8, 10, 12, 15],
+ "min_samples_leaf" : [ 1,2,3,4,5 ],
+"min_weight_fraction_leaf":[0.1,0.2,0.3,0.4],
+ "max_features" : ["auto","log2","sqrt",None ],
+    "max_leaf_nodes":[None,10,20,30,40,50,60,70]
+    
+}
+
+## Hyperparameter optimization using GridSearchCV
+from sklearn.model_selection import GridSearchCV
+dtree = DecisionTreeRegressor()
+random_search_dt=GridSearchCV(dtree,param_grid=params,scoring='r2',n_jobs=-1,cv=10,verbose=3)
+model_fit_DT=random_search_dt.fit(X_train, y_train)
+print(model_fit_DT.best_score_)
+
+predictions_dt=random_search_dt.predict(X_test)
+
+from sklearn import metrics
+
+print('DT Mean Absolute Error:', metrics.mean_absolute_error(y_test, predictions_dt))
+print('DT Mean Squared Error:', metrics.mean_squared_error(y_test, predictions_dt))
+print('DT R2:', metrics.r2_score(y_test, predictions_dt))
+print('DT Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, predictions_dt)))
+# Saving the model
+import pickle
+pickle.dump(model_fit_DT, open('papermil_dt.pkl', 'wb'))
+
+
+
